a5d35bd01e99040c5d0686c1b4ba59ffb1af2fe8
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-08-27 09:04:42 -0600

    update ManyToMany metodos

--- hibernate-jpa-asociaciones-app/src/main/java/org/hibernate/asociaciones/HibernateAsociacionesManyToManyFind.java
+++ hibernate-jpa-asociaciones-app/src/main/java/org/hibernate/asociaciones/HibernateAsociacionesManyToManyFind.java
@@ -10,32 +10,7 @@ public class HibernateAsociacionesManyToManyFind {
         EntityManager em = JpaUtil.getEntityManager();
         try {
             System.out.println("========= START: HibernateAsociacionesManyToManyFind =========");
-            em.getTransaction().begin();
-            Alumno alumno1 = new Alumno("Cata", "Edu");
-            Alumno alumno2 = new Alumno("Jano", "Fernan");
 
-            Curso curso1 = new Curso("Curso Java", "Andres");
-            Curso curso2 = new Curso("Jano", "Fernan");
-
-            alumno1.addCurso(curso1);
-            alumno1.addCurso(curso2);
-            alumno2.addCurso(curso1);
-
-            em.persist(alumno1);
-            em.persist(alumno2);
-
-            em.getTransaction().commit();
-
-            System.out.println(alumno1);
-            System.out.println(alumno2);
-
-            em.getTransaction().begin();
-            // c2 = em.find(Curso.class, 3L);
-            Curso c2 = new Curso("Curso Java", "Andres");
-            c2.setId(3L);
-            alumno1.removeCurso(c2);
-            em.getTransaction().commit();
-            System.out.println(alumno1);
         } catch (Exception e){
             em.getTransaction().rollback();
             e.printStackTrace();
